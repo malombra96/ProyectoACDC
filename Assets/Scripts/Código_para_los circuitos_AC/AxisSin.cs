4657742c4e60824215d7df50987dd2bcdb671c24
Brandon Malagon <malombrabmb@gmail.com>
2020-08-26 00:33:29 -0500

    Demo terminado y se organizo el proyecto

---
+++ Assets/Scripts/Código_para_los circuitos_AC/AxisSin.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Controla el comportamiento de los ejes de graficación
+/**
+	Toma las variables de entrada y de salida de cada circuito y las gráfica
+*/
+public class AxisSin : MonoBehaviour {
+	/// Objeto de Unity usado como referencia para la gráfica Roja
+	public  float reference1,reference2;
+	/// Objeto de Unity para instanciar la linea roja
+	LineRenderer axesLineRenderer1;
+	/// Objeto de Unity para instanciar la linea azul
+	LineRenderer axesLineRenderer2;
+	/// variable para almacenar la posición x de los ejes
+	float xp;
+	/// variable para almacenar la posición y de los ejes
+	float yp;
+	/// variable para almacenar la posición z de los ejes
+	float zp;
+	/// variable para almacenar la escala x de los ejes
+	float sx;
+	/// variable para almacenar la escala y de los ejes
+	float sy;
+	/// variable para almacenar el tiempo transcurrido, corresponde con la información del eje x
+	float lineTime;
+	/// variable para almacenar el ciclo del Update para graficar
+	int k;
+	/// variable para almacenar el signo de la señal azul
+	bool outSign = true;
+
+// =====================================================================================================
+/// Método Start. Se ejecuta una vez al iniciar la ejecución del programa
+/**
+  Inicializa las varibles, instancia las lineas para las gráficas, almacena la posición de los ejes.
+*/
+	void Start () {
+
+		// axesLineRenderer1 = gameObject.AddComponent<LineRenderer>();
+		axesLineRenderer1 = transform.GetChild(0).gameObject.AddComponent<LineRenderer>();
+		axesLineRenderer1.material = new Material(Shader.Find("Sprites/Default"));
+		axesLineRenderer1.material.color = Color.red;
+		axesLineRenderer1.startColor = Color.red;
+		axesLineRenderer1.startWidth = 0.03f;
+		axesLineRenderer1.positionCount = 1;
+
+		axesLineRenderer2 = transform.GetChild(1).gameObject.AddComponent<LineRenderer>();
+		axesLineRenderer2.material = new Material(Shader.Find("Sprites/Default"));
+		axesLineRenderer2.material.color = new Color(0.0F, 0.0F, 1.0F, 1F);
+		axesLineRenderer2.startColor = new Color(0.0F, 0.0F, 1.0F, 1F);
+		axesLineRenderer2.startWidth = 0.03f;
+		axesLineRenderer2.positionCount = 1;
+
+
+		xp = transform.position.x;
+		yp = transform.position.y;// - transform.localScale.z/2;
+		zp = transform.position.z - 0.01f;
+
+		sx = 5*transform.localScale.x * transform.parent.localScale.x;
+		sy = 5*transform.localScale.z * transform.parent.localScale.y;
+		
+		print("sx = " + sx);
+		print("transform.position.x"+transform.position.x);
+		print("transform.localScale.x = " + transform.localScale.x);
+		print("transform.parent.localScale.x = " + transform.parent.localScale.x);
+		
+		ResetLineAxes();
+
+	}
+// =====================================================================================================
+/// Método ResetLineAxes. Se ejecuta cada vez que se carga un nuevo circuito
+/**
+  Reinicia el tiempo, que correponde al eje X
+*/
+	public void ResetLineAxes(){
+
+		lineTime = 0;
+		k = 0;
+	}
+// =====================================================================================================
+/// Método ReferenceAssignment. Asigna la variable de entrada y de salida
+/**
+  \param O1 Objeto que representa la variable roja
+  \param O2 Objeto que representa la variable azul
+  \param sign variable que representa el signo de la variable azul
+*/
+	public void ReferenceAssignment (float O1, float O2, bool sign){
+
+		reference1 = O1;
+		reference2 = O2;
+		outSign = sign;
+}
+// =====================================================================================================
+/// Método Update. Se ejecuta una vez cada frame
+/**
+  Se encarga de graficar, actualiza la información de la entrada y la salida cada frame
+*/
+	void Update () {
+
+		lineTime += Time.deltaTime;
+		if(lineTime>20){
+			lineTime = 0;
+			k = 1;
+			axesLineRenderer1.positionCount = k;
+			axesLineRenderer2.positionCount = k;
+		}
+		else{
+			k++;
+		}
+
+		axesLineRenderer1.positionCount = k;
+		axesLineRenderer2.positionCount = k;
+
+		axesLineRenderer1.SetPosition(k-1,new Vector3(xp - sx + (lineTime/5)*(sx/2), yp-sy+reference1/1.5f*sy, zp));
+
+		if (outSign) {
+		    axesLineRenderer2.SetPosition(k-1,new Vector3(xp - sx + lineTime/5*sx/2, yp-sy+reference2/1.5f*sy, zp));
+		} 
+		else {
+			axesLineRenderer2.SetPosition(k-1,new Vector3(xp - sx + lineTime/5*sx/2, yp-sy-reference2/1.5f*sy, zp));
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Código_para_los circuitos_AC/DemoSin.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// Controla el comportamiento del demo
+/**
+
+*/
+public class DemoSin : MonoBehaviour
+{
+	// objeto de unity como referencia para la frecuencia angular
+	public Transform W; // frecuencia angular 
+	// variable para la frecuencia 
+	private float frecuencia;
+	// magnitud de la frecuencia angular
+	private float w;
+	//valores para recrear dos señal senosoidal 
+	private float signal_seno, signal_coseno;
+	// variable para determinar el periodo de la señal y generar el desfase entre las 2 señales 
+    private float periodo;
+	// objeto unity scope 
+    public GameObject axes;
+	// variable para generar el tiempo de las señales
+	private float linetime_s, linetime_c; 
+	// objeto unity aguja del medidor 
+	public Transform needle;
+	//varible interuptor para mostrar 1 o las 2 señales
+	public bool interuptor;
+// =====================================================================================================
+// inicializa todas las variables 
+// ======================================================================================================
+	void Start()
+	{
+		linetime_s = 0;     						// linea de tiempo en cero
+		w = W.localScale.y;							// frecuencia angular iniciada 
+		frecuencia = (w) / (2 * Mathf.PI);			// determinamos la frecuencia de las señales
+        periodo = 1 / frecuencia;					// obtenemos el periodo de las señales 
+	    signal_seno = Mathf.Sin(w);					// inicializamos la señal seno
+        linetime_c = periodo / 4;					//generamos el desfase entre las señales
+	}
+
+    // =====================================================================================================
+
+    void FixedUpdate()
+    {
+	    // enviamos los datos de cada señal cada 0.02 segundos 
+	    if (interuptor)
+	    {
+		    axes.GetComponent<AxisSin>().ReferenceAssignment(signal_seno, signal_seno, true);
+	    }
+	    else
+	    {
+		    axes.GetComponent<AxisSin>().ReferenceAssignment(signal_seno, signal_coseno, true);
+	    }
+	    linetime_c += Time.deltaTime;										//tiempo de la señal coseno
+        linetime_s += Time.deltaTime;										//tiempo de la señal seno 
+        w = W.localScale.y;													//actualizamos la frecuencia 
+		signal_coseno = Mathf.Sin(w*linetime_c);							//calculamos la señal coseno
+        signal_seno = Mathf.Sin(w * linetime_s);							//calculamos la señal seno
+        frecuencia = (w) / (2 * Mathf.PI);									//actualizamos la frecuencia
+        //linealizamos para generar el movimiento de la aguja del medidor
+        float angle = -(700/23)*w + (490 / 23);								
+		needle.localEulerAngles = new Vector3(90, 150, angle);	//generamos el movimiento de la aguja 
+    }
+
+
+}
+
