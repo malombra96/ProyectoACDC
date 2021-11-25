df083c3df7a973cf4d673558eb3bfd2ea959cf6e
Juan Ángel Quevedo <69872766+Jaquevedo@users.noreply.github.com>
2021-11-25 08:25:27 -0500

    Imagenes y circuito monofasico

--- Assets/Scripts/CircuitoMonofase.cs
+++ Assets/Scripts/CircuitoMonofase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CircuitoMonofase : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class CircuitoMonofase : MonoBehaviour
     //public Transform OVf;
     /// Objeto de Unity asociado a la corriente de linea Ia
     public Transform OIa;
+
+    /// Objetos de Unity asociados a la impedancia Z1
+	public Transform OZR, OZX;
+
     /// variable para almacenar la proporción altura/ancho de las letras
     float ratio;
     // magnitud de la frecuencia angular
@@ -30,10 +35,20 @@ public class CircuitoMonofase : MonoBehaviour
     public BehaviourReloj memoria;
     /// vector para almacenar la escala de las corriente
     private Vector3 IaScale;
-	
-// =====================================================================================================
-// inicializa todas las variables 
-// ======================================================================================================
+
+    /// valor real y reactancia del ZL
+	private float ZR;
+    private float ZX;
+    /// Representacion polar de ZL
+    private float MagZ;
+    private float FaseZ;
+
+    //lista de los selecctores
+    public List<Toggle> check;
+
+    // =====================================================================================================
+    // inicializa todas las variables 
+    // ======================================================================================================
     void Start()
     {
         signal_int1 = 0;     					              	// linea de tiempo en cero
@@ -49,23 +64,43 @@ public class CircuitoMonofase : MonoBehaviour
     {
 	    
 	    
-        axes.GetComponent<AxisSin>().ReferenceAssignment(signal_int1, signal_Out1, signal_Out1, signal_coseno2,1);
+        axes.GetComponent<AxisSin>().ReferenceAssignment(signal_int1, signal_Out1, signal_Out1, signal_coseno2,2);
 	    
         linetime_int1 += Time.deltaTime;										//tiempo de la señal seno 
         linetime_int2 += Time.deltaTime;										//tiempo de la señal seno 
         linetime_Out1 += Time.deltaTime;										//tiempo de la señal seno 
 
-        V_1 = V1.localScale.y; 												//actualizamos la frecuencia 
-        //V_2 = V2.localScale.y; 												//actualizamos la frecuencia 
-	    
-        float amplitud1 = 2.04f * V_1 - 1.33f;
-        float amplitud2 = 2.04f * V_2 - 1.33f;
-        float amplitud3 = amplitud1 + amplitud2;
-        
-        signal_int1 = amplitud1*Mathf.Sin(memoria.dato * linetime_int1);		//calculamos la señal seno
-        signal_int2 = amplitud2*Mathf.Sin(memoria.dato * linetime_int2);		//calculamos la señal seno
-        signal_Out1 = amplitud3*Mathf.Sin(memoria.dato * linetime_Out1);		//calculamos la señal seno
-        
+        V_1 = V1.localScale.y;                                              //actualizamos la frecuencia 
+                                                                            //V_2 = V2.localScale.y; 												//actualizamos la frecuencia 
+
+        //float amplitud1 = 2.04f * V_1 - 1.33f;
+        //float amplitud2 = 2.04f * V_2 - 1.33f;
+        //float amplitud3 = amplitud1 + amplitud2;
+
+        //Se adquieren las escalas de los objetos para las impedancias Z1, Z2 y Z3. 
+
+
+        CheckToggle();
+
+        if (ZR == 0.5f)
+        {
+            ZR = 0;
+        }
+
+        if (ZX == 0.5f)
+        {
+            ZX = 0;
+        }
+
+        //Se hace una representacion polar para cada una de las impedancias.
+        MagZ = Mathf.Sqrt(Mathf.Pow(ZR, 2) + Mathf.Pow(ZX, 2));
+        FaseZ = Mathf.Atan2(ZX, ZR);
+
+
+        signal_int1 = V_1*Mathf.Sin(memoria.dato * linetime_int1);		//calculamos la señal seno
+        //signal_int2 = amplitud2*Mathf.Sin(memoria.dato * linetime_int2);		//calculamos la señal seno
+        signal_Out1 = V_1*Mathf.Sin(memoria.dato * linetime_Out1 + FaseZ);		//calculamos la señal seno
+
         //OVf.localScale = new Vector3( signal_Out1,signal_Out1,signal_Out1);
         //OVf.position = Vcpos - new Vector3(0, signal_Out1/2, 0);
         if (signal_int1 > 0)
@@ -77,5 +112,21 @@ public class CircuitoMonofase : MonoBehaviour
             OIa.localScale = new Vector3(-0.01f, 0.01f, signal_int1 * 1 / 100);
         }
         
+    }
+
+    public void CheckToggle()
+    {
+        if (check[0].isOn)
+        {
+            ZR = OZR.localScale.y;
+            ZX = OZX.localScale.y;
+        }
+
+        if (check[1].isOn)
+        {
+            ZR = OZR.localScale.y;
+            ZX = - OZX.localScale.y;
+        }
+
     }
 }
